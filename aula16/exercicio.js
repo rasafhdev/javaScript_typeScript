29cd85a30e1c57491569c00331b6263b7ed1b752
Rodrigo Asafh <rasafh.dev@gmail.com>
2025-01-05 13:24:30 -0300

    Create exercicio.js

---
+++ aula16/exercicio.js
@@ -0,0 +1,14 @@
+/*
+Faça um programa com const e let para calcular o IMC
+*/
+
+const name = 'Rodrigo'
+const secondName = 'Asafh'
+const idade = '28'
+const peso = '82'
+const altura = '1.71'
+let imc
+
+imc = peso / (altura * altura)
+
+console.log(`${imc}`)
